60c025413f3d1ea6dfd7a5c7f82dd78ad26b17ce
Sarosh Ranji <Sarosh.Ranji1@marist.edu>
2018-01-29 17:49:32 -0500

    Solution to Curiosity rover exercise.

---
+++ Rover.py
@@ -0,0 +1,8 @@
+#Mars Rover
+def main():
+    print("Time for Mars rover photos to transmit to NASA")
+    distance = 34000000
+    speed = 186000
+    print ("time of travel is", distance/speed, "in seconds")
+
+main()
