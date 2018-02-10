e6714d419564c39bfe4d7aac87363d37d4514105
SaroshRanji <35709457+SaroshRanji@users.noreply.github.com>
2018-02-09 22:19:10 -0500

    Create Pi.py

---
+++ Pi.py
@@ -0,0 +1,13 @@
+#Fibonacci
+
+def fib(n):
+    print("This is a way to show the fibonacci sequence.")
+    n = eval(input("Input number to calculate the Fibonacci"))
+    a,b = 1,1
+ for i in range(n):
+     print (a)
+     #c = a + b
+     #a = b
+     #b = c
+     a,b = b,a+b
+fib()
