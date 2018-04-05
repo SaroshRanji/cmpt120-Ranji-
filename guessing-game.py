209b60e77d12a1eea243b941c533e9f02510915e
Sarosh Ranji <Sarosh.Ranji1@marist.edu>
2018-04-05 10:52:46 -0400

    Usability improvements to the guessing game, resolves #2 and #3.

--- guessing-game.py
+++ guessing-game.py
@@ -3,11 +3,14 @@
 def main():
     animal = ("penguin")
     print ("I'm thinking of an animal")
-    guess = input("Guess the animal") #prints text and lets user guess
+    guess = input("Guess the animal").lower() #prints text and lets user guess
 
     while True:
         if guess == animal:
             print("You guessed correctly!")
+            break
+        elif guess == "quit":
+            print("Goodbye")
             break
         else:
             print("Incorrect.")
